807568ec58b35c13d3c60a0106c32f006598b5b5
Victor QUESNEL <vquesnel@student.42.fr>
2016-06-21 15:20:47 +0200

    ok

---
+++ j07/ex06/Fighter.class.php
@@ -0,0 +1,9 @@
+<?php
+	abstract Class Fighter {
+			public $class;
+			public function __construct($class) {
+						$this->class = $class;
+					}
+			abstract public function fight($target);
+		}
+?>
